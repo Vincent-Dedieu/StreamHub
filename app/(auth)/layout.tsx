90be26cd6baeeeccf243877297c8f1e92a47902d
Vincent <d.vincent1@hotmail.fr>
2023-12-19 20:21:19 +0700

    adding logo.svg on auth page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,12 @@
+import { Logo } from "./_components/Logo";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="h-full flex items-center justify-center">{children}</div>;
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Logo />
+      {children}
+    </div>
+  );
 };
 
 export default AuthLayout;
