e5d68196f16a776e570bc93fe7064e45cf785656
Vincent <d.vincent1@hotmail.fr>
2023-12-23 18:36:19 +0700

    fix

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -16,7 +16,7 @@ export const Logo = () => {
           <Image src="/logo.svg" alt="streamhub" height="32" width="32" />
         </div>
         <div className={cn(font.className)}>
-          <p className="text-xl font-semibold">Gamehub</p>
+          <p className="text-xl font-semibold">Streamhub</p>
           <p className="text-xs text-muted-foreground">Let&apos;s play</p>
         </div>
       </div>
