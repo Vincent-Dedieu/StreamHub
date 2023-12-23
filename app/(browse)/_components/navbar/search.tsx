5733885ec710a5a737201994638e7aa62b4ea57f
Vincent <d.vincent1@hotmail.fr>
2023-12-23 18:33:35 +0700

    search component - query-string for url

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -1,7 +1,47 @@
 "use client";
+import qs from "query-string";
+import React, { useState } from "react";
+import { SearchIcon, X } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const Search = () => {
-  return <div>search</div>;
+  const router = useRouter();
+  const [value, setValue] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!value) return;
+
+    const url = qs.stringifyUrl({ url: "/", query: { q: value } }, { skipEmptyString: true });
+    router.push(url);
+  };
+
+  const onClear = () => {
+    setValue("");
+  };
+
+  return (
+    <form className="relative w-full flex items-center lg:w-[400px]" onSubmit={onSubmit}>
+      <Input
+        className=" focus-visible:ring-transparent"
+        placeholder="Search..."
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {value && (
+        <X
+          className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground hover:opacity-50 transition"
+          onClick={onClear}
+        />
+      )}
+      <Button type="submit" size="sm" variant="secondary">
+        <SearchIcon className="h-5 w-5 text-m" />
+      </Button>
+    </form>
+  );
 };
 
 export default Search;
