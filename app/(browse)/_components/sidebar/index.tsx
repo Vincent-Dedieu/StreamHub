05e8f5f11d21ceac0044e07a9ebb8a61e576fd96
Vincent <d.vincent1@hotmail.fr>
2023-12-26 18:05:05 +0700

    Recommanded users component

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,10 +1,16 @@
+import { getRecommanded } from "@/lib/recommanded-service";
+import Recommanded from "./recommanded";
 import Toggle from "./toggle";
 import Wrapper from "./wrapper";
 
-const Sidebar = () => {
+const Sidebar = async () => {
+  const recommanded = await getRecommanded();
   return (
     <Wrapper>
       <Toggle />
+      <div className="space-y-4 pt-4 lg:pt-0">
+        <Recommanded data={recommanded} />
+      </div>
     </Wrapper>
   );
 };
---
+++ app/(browse)/_components/sidebar/recommanded.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useSidebar } from "@/store/use-sidebar";
+import { User } from "@prisma/client";
+import UserItem from "./user-item";
+
+interface RecommandedProps {
+  data: User[];
+}
+
+const Recommanded = ({ data }: RecommandedProps) => {
+  const { collapsed } = useSidebar((state) => state);
+
+  const showLabel = !collapsed && data.length > 0;
+
+  return (
+    <div>
+      {showLabel && (
+        <div className="pl-6 mb-4">
+          <p className=" text-sm text-muted-foreground">Recommanded</p>
+        </div>
+      )}
+      <ul className="space-y-2 px-2">
+        {data.map((user) => (
+          <UserItem key={user.id} username={user.username} imageUrl={user.imageUrl} isLive={true} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Recommanded;
