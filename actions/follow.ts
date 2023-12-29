d98c744443e8a301154ef7557896641dd20f0d15
Vincent <d.vincent1@hotmail.fr>
2023-12-29 20:29:24 +0700

    unfollowUser fonction updated

--- actions/follow.ts
+++ actions/follow.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { followUser } from "@/lib/follow-service";
+import { followUser, unfollowUser } from "@/lib/follow-service";
 import { revalidatePath } from "next/cache";
 
 export const onFollow = async (id: string) => {
@@ -14,6 +14,22 @@ export const onFollow = async (id: string) => {
     }
 
     return followedUser;
+  } catch (error) {
+    throw new Error("error");
+  }
+};
+
+export const onUnfollow = async (id: string) => {
+  try {
+    const unfollowedUser = await unfollowUser(id);
+
+    revalidatePath("/");
+
+    if (unfollowedUser) {
+      revalidatePath(`/${unfollowedUser.following.username}`);
+    }
+
+    return unfollowedUser;
   } catch (error) {
     throw new Error("error");
   }
