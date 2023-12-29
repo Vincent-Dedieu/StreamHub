27a068f42cda6f6ef8407aec102ae2ca4fb2a969
Vincent <d.vincent1@hotmail.fr>
2023-12-29 20:37:12 +0700

    handleFollow handleUnfollow functions frontend ok

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { onFollow } from "@/actions/follow";
+import { onFollow, onUnfollow } from "@/actions/follow";
 import { Button } from "@/components/ui/button";
 import { useTransition } from "react";
 import { toast } from "sonner";
@@ -13,7 +13,7 @@ interface ActionsProps {
 export const Actions = ({ isFollowing, userId }: ActionsProps) => {
   const [isPending, startTransition] = useTransition();
 
-  const onClick = () => {
+  const handleFollow = () => {
     startTransition(() => {
       onFollow(userId)
         .then((data) => toast.success(`You just followed ${data.following.username}`))
@@ -21,9 +21,19 @@ export const Actions = ({ isFollowing, userId }: ActionsProps) => {
     });
   };
 
+  const handleUnfollow = () => {
+    startTransition(() => {
+      onUnfollow(userId)
+        .then((data) => toast.success(`You just unfollowed ${data.following.username}`))
+        .catch(() => toast.error("Failed to unfollow user."));
+    });
+  };
+
+  const onClick = isFollowing ? handleUnfollow : handleFollow;
+
   return (
-    <Button onClick={onClick} disabled={isPending || isFollowing} variant="primary">
-      Follow
+    <Button onClick={onClick} disabled={isPending} variant="primary">
+      {isFollowing ? "Unfollow" : "Follow"}
     </Button>
   );
 };
