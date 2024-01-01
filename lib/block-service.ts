18383182903fa13528a970c855e5dd5473bebf6f
Vincent <d.vincent1@hotmail.fr>
2024-01-01 16:59:02 +0700

    block-service.ts

---
+++ lib/block-service.ts
@@ -0,0 +1,74 @@
+import getSelf from "./auth-service";
+import { db } from "./db";
+
+export const isBlockedByUser = async (id: string) => {
+  try {
+    const self = await getSelf();
+
+    const otherUser = await db.user.findUnique({
+      where: { id },
+    });
+
+    if (!otherUser) {
+      throw new Error("User not found");
+    }
+    if (otherUser.id === self.id) {
+      // You can't block yourself
+      return false;
+    }
+
+    const existingBlock = await db.block.findUnique({
+      where: {
+        blockerId_blockedId: {
+          blockerId: otherUser.id,
+          blockedId: self.id,
+        },
+      },
+    });
+
+    return !!existingBlock;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const blockUser = async (id: string) => {
+  const self = await getSelf();
+
+  if (self.id === id) {
+    throw new Error("You can't block yourself");
+  }
+
+  const otherUser = await db.user.findUnique({
+    where: { id },
+  });
+
+  if (!otherUser) {
+    throw new Error("User not found");
+  }
+
+  const existingBlock = await db.block.findUnique({
+    where: {
+      blockerId_blockedId: {
+        blockerId: self.id,
+        blockedId: otherUser.id,
+      },
+    },
+  });
+
+  if (existingBlock) {
+    throw new Error("You have already blocked this user");
+  }
+
+  const block = await db.block.create({
+    data: {
+      blockerId: self.id,
+      blockedId: otherUser.id,
+    },
+    include: {
+      blocked: true,
+    },
+  });
+
+  return block;
+};
