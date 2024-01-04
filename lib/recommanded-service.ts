c9a731ff774ae3e90c78692ffaa376e93f30991e
Vincent <d.vincent1@hotmail.fr>
2024-01-04 14:39:07 +0700

    fix ok1 block/unblock service

--- lib/recommanded-service.ts
+++ lib/recommanded-service.ts
@@ -1,56 +1,6 @@
 import { db } from "./db";
 import getSelf from "./auth-service";
 
-// export const getRecommanded = async () => {
-//   let userId;
-
-//   try {
-//     const self = await getSelf();
-//     userId = self.id;
-//   } catch (error) {
-//     userId = null;
-//   }
-
-//   let users = [];
-
-//   if (userId) {
-//     // Get the IDs of the users followed by the current user
-//     const followedUserIds = (
-//       await db.follow.findMany({
-//         where: { followerId: userId },
-//         select: { followingId: true },
-//       })
-//     ).map((follow) => follow.followingId);
-
-//     // Get the IDs of the users blocked by the current user
-//     const blockedUsersIds = (
-//       await db.block.findMany({
-//         where: { blockerId: userId },
-//         select: { blockedId: true },
-//       })
-//     ).map((block) => block.blockedId);
-
-//     // Use these IDs to exclude these users from the findMany query
-//     users = await db.user.findMany({
-//       where: {
-//         AND: [{ id: { notIn: [userId, ...followedUserIds, ...blockedUsersIds] } }],
-//       },
-//       orderBy: {
-//         createdAt: "desc",
-//       },
-//     });
-//   } else {
-//     // If no user is logged in, retrieve all users
-//     users = await db.user.findMany({
-//       orderBy: {
-//         createdAt: "desc",
-//       },
-//     });
-//   }
-
-//   return users;
-// };
-
 export const getRecommanded = async () => {
   let userId;
 
