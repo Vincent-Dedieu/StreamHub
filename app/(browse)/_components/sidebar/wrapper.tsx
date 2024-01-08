eeb8e98f4494984a1c3684581248ded43b71703e
Vincent <d.vincent1@hotmail.fr>
2024-01-08 12:31:42 +0700

    creator dashboard + wrapper(hydration fix) + container(responsive)

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -34,8 +34,8 @@ const Wrapper = ({ children }: WrapperProps) => {
   return (
     <aside
       className={cn(
-        "fixed left-0 flex flex-col h-full bg-background border-r border-[#2D2E35] z-50",
-        collapsed && "w-[70px]"
+        "fixed left-0 flex flex-col h-full w-[70px] lg:w-60 bg-background border-r border-[#2D2E35] z-50",
+        collapsed && "lg:w-[70px]"
       )}
     >
       {children}
--- app/(dashboard)/u/[username]/layout.tsx
+++ app/(dashboard)/u/[username]/layout.tsx
@@ -1,6 +1,8 @@
 import { getSelfByUsername } from "@/lib/auth-service";
 import { redirect } from "next/navigation";
 import Navbar from "./_components/navbar";
+import Sidebar from "./_components/sidebar";
+import Container from "./_components/container";
 
 interface CreatorLayoutProps {
   params: { username: string };
@@ -17,7 +19,10 @@ const CreatorLayout = async ({ params, children }: CreatorLayoutProps) => {
   return (
     <>
       <Navbar />
-      <div className="flex h-full pt-20">{children}</div>
+      <div className="flex h-full pt-20">
+        <Sidebar />
+        <Container>{children}</Container>
+      </div>
     </>
   );
 };
