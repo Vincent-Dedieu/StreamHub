52595a32a200489675854953775998790c70936a
Vincent <d.vincent1@hotmail.fr>
2024-01-16 18:10:55 +0700

    server action stream.ts

--- app/(dashboard)/u/[username]/chat/page.tsx
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -1,8 +1,24 @@
-const ChatPage = () => {
+import getSelf from "@/lib/auth-service";
+import { getStreamByUserId } from "@/lib/stream-service";
+import { ToggleCard } from "./_components/toggle-card";
+
+const ChatPage = async () => {
+  const self = await getSelf();
+  const stream = await getStreamByUserId(self.id);
+
+  if (!stream) {
+    throw new Error("Stream not found");
+  }
+
   return (
     <div className="p-6">
       <div className="mb-4">
         <h1 className="text-2xl font-bold">Chat settings</h1>
+      </div>
+      <div className="space-y-4">
+        <ToggleCard field="isChatEnabled" label="Enable chat" value={stream.isChatEnabled} />
+        <ToggleCard field="isChatDelayed" label="Delayed chat" value={stream.isChatDelayed} />
+        <ToggleCard field="isChatFollowersOnly" label="Enable followers only" value={stream.isChatFollowersOnly} />
       </div>
     </div>
   );
