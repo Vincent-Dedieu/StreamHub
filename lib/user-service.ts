d00ee4b024127b5c9a99d825f7d3226d4610110e
Vincent <d.vincent1@hotmail.fr>
2024-01-27 18:14:48 +0700

    use-viewer-token with server action token.ts

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -14,3 +14,18 @@ export const getUserByUsername = async (username: string) => {
 
   return user;
 };
+
+export const getUserById = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: { id },
+    include: {
+      stream: true,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return user;
+};
