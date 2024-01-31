f87f1225b6614d481818594ab13e26c68b60126a
Vincent <d.vincent1@hotmail.fr>
2024-01-31 13:40:21 +0700

    fullscreen control

---
+++ components/stream-player/fullscreen-control.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Maximize, Minimize } from "lucide-react";
+
+import { Hint } from "../hint";
+
+export interface FullscreenControlProps {
+  isFullscreen: boolean;
+  onToggle: () => void;
+}
+
+export const FullScreenControl = ({ isFullscreen, onToggle }: FullscreenControlProps) => {
+  const Icon = isFullscreen ? Minimize : Maximize;
+  const label = isFullscreen ? "Exit fullscreen" : "Enter fullscreen";
+
+  return (
+    <div className="flex items-center justify-center gap-4">
+      <Hint label={label} asChild>
+        <button className="" onClick={onToggle}>
+          <Icon className="w-6 h-6 text-white transform hover:scale-125 transition-transform duration-200" />
+        </button>
+      </Hint>
+    </div>
+  );
+};
--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -3,6 +3,7 @@
 import { Participant, Track } from "livekit-client";
 import { useRef } from "react";
 import { useTracks } from "@livekit/components-react";
+import { FullScreenControl } from "./fullscreen-control";
 
 interface LiveVideoProps {
   participant: Participant;
@@ -23,6 +24,11 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   return (
     <div className="relative h-full flex" ref={wrapperRef}>
       <video ref={videoRef} width="100%" />
+      <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
+        <div className="absolute  flex bottom-0 left-96 w-full items-center justify-between bg-transparent px-4">
+          <FullScreenControl isFullscreen={false} onToggle={() => {}} />
+        </div>
+      </div>
     </div>
   );
 };
