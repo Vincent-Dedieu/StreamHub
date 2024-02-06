eaed7354ad8949be1bc77f91bdc66c03500d64a1
Vincent <d.vincent1@hotmail.fr>
2024-02-06 12:27:06 +0700

    chat

---
+++ components/stream-player/chat-header.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Skeleton } from "../ui/skeleton";
+
+export const ChatHeader = () => {
+  return (
+    <div className="relative p-3 border-b">
+      <p className="font-semibold text-center">Stream Chat</p>
+    </div>
+  );
+};
+
+export const ChatHeaderSkeleton = () => {
+  return (
+    <div className="relative p-3 border-b hidden md:block">
+      <Skeleton className="w-6 h-6 left-3 top-3" />
+      <Skeleton className="w-28 h-6 mx-auto" />
+    </div>
+  );
+};
--- components/stream-player/chat.tsx
+++ components/stream-player/chat.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import { ChatHeader } from "./chat-header";
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+import { useEffect, useMemo, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+import { ConnectionState } from "livekit-client";
+import { useChat, useConnectionState, useRemoteParticipant } from "@livekit/components-react";
+
 interface ChatProps {
   viewerName: string;
   hostName: string;
@@ -19,5 +26,41 @@ export const Chat = ({
   isChatFollowersOnly,
   isFollowing,
 }: ChatProps) => {
-  return <div>Chat</div>;
+  const matches = useMediaQuery("(max-width: 1024px)");
+  const { variant, onExpand } = useChatSidebar((state) => state);
+  const connectionState = useConnectionState();
+  const participant = useRemoteParticipant(hostIdentity);
+
+  const isOnline = participant && connectionState === ConnectionState.Connected;
+  const isHidden = !isChatEnabled || !isOnline;
+
+  const [value, setValue] = useState("");
+  const { chatMessages: messages, send } = useChat();
+
+  useEffect(() => {
+    if (matches) {
+      onExpand();
+    }
+  }, [matches, onExpand]);
+
+  const reversedMessages = useMemo(() => {
+    return messages.sort((a, b) => b.timestamp - a.timestamp);
+  }, [messages]);
+
+  const onSubmit = () => {
+    if (!send) return;
+
+    send(value);
+    setValue("");
+  };
+
+  const onChange = (value: string) => {
+    setValue(value);
+  };
+
+  return (
+    <div className="flex flex-col bg-background border-b h-[calc(100vh-80px)]">
+      <ChatHeader />
+    </div>
+  );
 };
