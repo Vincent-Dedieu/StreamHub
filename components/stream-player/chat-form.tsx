7f99dd88b166d3fb3d7ec6e9a6261f12017c9b68
Vincent <d.vincent1@hotmail.fr>
2024-02-07 16:02:09 +0700

    chat form

---
+++ components/stream-player/chat-form.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Skeleton } from "../ui/skeleton";
+import React, { useState } from "react";
+
+interface ChatFormProps {
+  value: string;
+  onChange: (value: string) => void;
+  onSubmit: () => void;
+  isHidden: boolean;
+  isFollowersOnly: boolean;
+  isFollowing: boolean;
+  isDelayed: boolean;
+}
+
+export const ChatForm = ({
+  value,
+  onChange,
+  onSubmit,
+  isHidden,
+  isFollowersOnly,
+  isDelayed,
+  isFollowing,
+}: ChatFormProps) => {
+  const [isDelayBlocked, setIsDelayBlocked] = useState(false);
+  const isFollowersOnlyAndNotFollowing = isFollowersOnly && !isFollowing;
+  const isDisabled = isHidden || isFollowersOnlyAndNotFollowing || isDelayBlocked;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!value || isDisabled) return;
+
+    if (isDelayed && !isDelayBlocked) {
+      setIsDelayBlocked(true);
+      setTimeout(() => {
+        setIsDelayBlocked(false);
+        onSubmit();
+      }, 3000);
+    } else {
+      onSubmit();
+    }
+  };
+
+  return (
+    <form className="flex flex-col items-center gap-y-4 p-3" onSubmit={handleSubmit}>
+      <div className="w-full">
+        <Input
+          className={cn("border-white/10", isFollowersOnly && "rounded-t-none border-t-0")}
+          onChange={(e) => onChange(e.target.value)}
+          value={value}
+          disabled={false}
+          placeholder="Send a message..."
+        />
+      </div>
+      <div className="ml-auto">
+        <Button type="submit" variant="primary" size="sm" disabled={false}>
+          Chat
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export const ChatFormSkeleton = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4 p-3">
+      <Skeleton className="w-full h-10" />
+      <div className="flex items-center gap-x-2 ml-auto">
+        <Skeleton className="w-7 h-7" />
+        <Skeleton className="w-7 h-12" />
+      </div>
+    </div>
+  );
+};
--- components/stream-player/chat.tsx
+++ components/stream-player/chat.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { ChatHeader } from "./chat-header";
-import { useChatSidebar } from "@/store/use-chat-sidebar";
+import { ChatVariant, useChatSidebar } from "@/store/use-chat-sidebar";
 import { useEffect, useMemo, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { ConnectionState } from "livekit-client";
 import { useChat, useConnectionState, useRemoteParticipant } from "@livekit/components-react";
+import { ChatForm } from "./chat-form";
 
 interface ChatProps {
   viewerName: string;
@@ -61,6 +62,24 @@ export const Chat = ({
   return (
     <div className="flex flex-col bg-background border-b h-[calc(100vh-80px)]">
       <ChatHeader />
+      {variant === ChatVariant.CHAT && (
+        <>
+          <ChatForm
+            value={value}
+            onSubmit={onSubmit}
+            onChange={onChange}
+            isHidden={isHidden}
+            isFollowersOnly={isChatFollowersOnly}
+            isDelayed={isChatDelayed}
+            isFollowing={isFollowing}
+          />
+        </>
+      )}
+      {variant === ChatVariant.COMMUNITY && (
+        <>
+          <p>Community mode</p>
+        </>
+      )}
     </div>
   );
 };
