4aeb2e2d05bfae537b72a712b5c0b24197708ad2
Vincent <d.vincent1@hotmail.fr>
2024-02-16 13:23:41 +0700

    stream header - badge+viewercount

---
+++ components/stream-player/header.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useParticipants, useRemoteParticipant } from "@livekit/components-react";
+import { UserAvatar } from "../user-avatar";
+import { VerifiedMark } from "../verified-mark";
+import { UserIcon } from "lucide-react";
+
+interface HeaderProps {
+  hostName: string;
+  hostIdentity: string;
+  viewerIdentity: string;
+  imageUrl: string;
+  isFollowing: boolean;
+  name: string;
+}
+
+export const Header = ({ hostName, hostIdentity, viewerIdentity, imageUrl, isFollowing, name }: HeaderProps) => {
+  const participants = useParticipants();
+  const participant = useRemoteParticipant(hostIdentity);
+
+  const isLive = !!participant;
+  const participantCount = participants.length - 1; // subtract 1 to exclude the host locally streaming
+  const hostAsViewer = `host-${hostIdentity}`;
+  const isHost = viewerIdentity === hostIdentity;
+
+  return (
+    <div className="flex flex-col lg:flex-row gap-y-4 lg:gap-y-0 items-start justify-between px-4">
+      <div className="flex items-center gap-x-3">
+        <UserAvatar imageUrl={imageUrl} username={hostName} size="lg" isLive={isLive} showBadge />
+        <div className="space-y-1">
+          <div className="flex items-center gap-x-2">
+            <h2 className="text-lg font-semibold">{hostName} </h2>
+            <VerifiedMark />
+          </div>
+          <p className="font-semibold text-xs"> {name}</p>
+          {isLive ? (
+            <div className="font-semibold flex gap-x-1 items-center text-xs text-rose-400">
+              <UserIcon className="h-4 w-4" />
+              <p>
+                {participantCount} {participantCount === 1 ? "viewer" : "viewers"}
+              </p>
+            </div>
+          ) : (
+            <p className="font-semibold text-xs text-muted-foreground">Offline</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -8,6 +8,7 @@ import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { cn } from "@/lib/utils";
 import { Chat } from "./chat";
 import { ChatToggle } from "./chat-toggle";
+import { Header } from "./header";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -40,6 +41,14 @@ export const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) =
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10 ">
           <Video hostName={user.username} hostIdentity={user.id} />
+          <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
         </div>
         <div className={cn("col-span-1", collapsed && "hidden")}>
           <Chat
